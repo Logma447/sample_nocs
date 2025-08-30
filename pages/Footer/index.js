2a7981386c8c73fe00fb85a5cc95267f24e13906
Nocomora-Aj <mustardjar.03@gmail.com>
2025-08-30 12:08:07 +0800

    my project

---
+++ pages/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return(
+        <div>
+            <h1 classname="text-red-800">Footer,Danger</h1>
+        </div>
+    )
+
+}
+    export default Footer
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import Navbar from "./Navbar";
 
 
 export default function Home() {
   return (
     <div>
-      <h1>helllo</h1>
+      <h1>hellooo</h1>
+      <Navbar/>
   
     </div>
   );
